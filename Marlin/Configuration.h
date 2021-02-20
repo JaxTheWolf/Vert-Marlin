4039f74f20244d5d0ab5d0214448c711eb62b554
vert <vert@vert.com>
2021-02-20 17:18:54 -0500

    Start of B504

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -76,7 +76,7 @@
 // Author info of this build printed to the host during boot and M115
 #define STRING_CONFIG_H_AUTHOR "(Vert)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
-#define SHORT_BUILD_VERSION "Bugfix Build 503"
+#define SHORT_BUILD_VERSION "Bugfix Build 504"
 
 //START_HERE
 //-------------------------
